c75c0841995e23cf21556c5db55eadd106326b1b
Sumit <sumit@appdios.com>
2013-06-14 23:01:03 -0700

    selection view added

---
+++ Phykids/Phykids/Scene/ADSelectionView.h
@@ -0,0 +1,13 @@
+//
+//  ADSelectionView.h
+//  Phykids
+//
+//  Created by Aditi Kamal on 6/14/13.
+//  Copyright (c) 2013 Appdios Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADSelectionView : UIView
+
+@end
